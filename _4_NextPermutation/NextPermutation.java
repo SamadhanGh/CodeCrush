459682949944144aea9eeb7f198208b558f308ef
samadhan <samadhanghorpade173@gmail.com>
2025-03-19 07:13:12 +0530

    NextPermutation

---
+++ _4_NextPermutation/NextPermutation.java
@@ -0,0 +1,52 @@
+package _4_NextPermutation;
+
+import java.util.*;
+
+public class NextPermutation{
+    static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    static void reverse(int[] nums, int start) {
+        int end = nums.length - 1;
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    static void nextPermutation(int[] nums) {
+        int idx1 = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx1 = i;
+                break;
+            }
+        }
+
+        if (idx1 == -1) {
+            reverse(nums, 0);
+            return;
+        }
+
+        int idx2 = -1;
+        for (int i = nums.length - 1; i > idx1; i--) {
+            if (nums[i] > nums[idx1]) {
+                idx2 = i;
+                break;
+            }
+        }
+
+        swap(nums, idx1, idx2);
+        reverse(nums, idx1 + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        nextPermutation(nums);
+        System.out.println("Next Permutation: " + Arrays.toString(nums));
+    }
+}
